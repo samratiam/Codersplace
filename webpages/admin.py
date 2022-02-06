d2009dd97733f701d0065770316ac475eeed4427
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-02-06 21:23:49 +0545

    Project Initialization

---
+++ webpages/admin.py
@@ -0,0 +1,7 @@
+import imp
+from django.contrib import admin
+from .models import Slider, Team
+# Register your models here.
+
+admin.site.register(Slider)
+admin.site.register(Team)
---
+++ webpages/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Slider, Team
+# Create your views here.
+def home(request):
+    sliders = Slider.objects.all()
+    teams = Team.objects.all()
+    data = {
+        'sliders': sliders,
+        'teams' : teams,
+    }
+    return render(request, 'webpages/home.html',data)
+
+def about(request):
+    return render(request, 'webpages/about.html')
+
+def contact(request):
+    return render(request, 'webpages/contact.html')
+
