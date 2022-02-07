89d351bfa3719753f4bcc24d41308195c3a84fd0
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-02-07 10:35:25 +0545

    Create model Coder

--- coders/models.py
+++ coders/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from datetime import datetime
+from ckeditor.fields import RichTextField
 
 # Create your models here.
+class Coder(models.Model):
+    level_choices = (
+        ('fresher','fresher'),
+        ('junior','junior'),
+        ('mid-level','mid-level'),
+        ('senior','senior'),
+    )
+
+    job_choices = (
+        ('part-time','part-time'),
+        ('full-time','full-time'),
+        ('remote-work','remote-work'),
+        ('intership','internship'),
+    )
+
+    developer_choices = (
+            ('.NET','.NET'),
+            ('React','React'),
+            ('Nodejs','Nodejs'),
+            ('PHP','PHP'),
+            ('Laravel','Laravel'),
+            ('Angular','Angular'),
+            ('Django','Django'),
+        )
+
+    name = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to = 'media/coders/')
+    video_url = models.CharField(max_length=255)
+    description = RichTextField()
+    city = models.CharField(max_length=255)
+    level_type = models.CharField(max_length=255, choices=level_choices)
+    job_type = models.CharField(max_length=255,choices=job_choices)
+    developer_type = models.CharField(max_length=255,choices=developer_choices)
+    is_featured = models.BooleanField(default=False)
+    created_date = models.DateTimeField(default=datetime.now,blank=True)
--- coders/urls.py
+++ coders/urls.py
@@ -1,2 +1,7 @@
 from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path("",views.coders,name="coders")
+]
 
