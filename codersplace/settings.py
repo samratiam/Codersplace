1d7e469651c1b866a2ce4601eccbb07c4ca01e0d
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-02-07 14:38:11 +0545

    Create hirecoders app and handle hire form

--- codersplace/settings.py
+++ codersplace/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'ckeditor',
     'webpages',
     'coders',
+    'hirecoders',
     'accounts',
 ]
 
--- codersplace/urls.py
+++ codersplace/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webpages.urls')),
     path('coders/', include('coders.urls')),
+    path('hirecoders/', include('hirecoders.urls')),
     path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
