eed562cc1fa44c9ff5b106012e1253feae8b88f8
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-02-07 10:15:50 +0545

    Create and setup coders app

--- coders/urls.py
+++ coders/urls.py
@@ -1,6 +1,2 @@
 from django.urls import path, include
-from . import views
 
-urlpatterns = [
-    path("",views.coders, name="coders"),
-]
--- coders/views.py
+++ coders/views.py
@@ -1,7 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
-def coders(request):
-    return HttpResponse("Coders page")
