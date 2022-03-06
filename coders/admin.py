925ad1c24c6f1d31e84041347f27b93f920edf0e
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-03-06 18:52:24 +0545

    Add skill field of multiple choices

--- coders/admin.py
+++ coders/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Coder
+from .models import Coder, Skill
 
 # Register your models here.
 admin.site.register(Coder)
+admin.site.register(Skill)
--- coders/models.py
+++ coders/models.py
@@ -3,41 +3,52 @@ from datetime import datetime
 from ckeditor.fields import RichTextField
 
 # Create your models here.
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Coder(models.Model):
     level_choices = (
-        ('Fresher','Fresher'),
-        ('Junior','Junior'),
-        ('Mid-level','Mid-level'),
-        ('Senior','Senior'),
+        ('Fresher', 'Fresher'),
+        ('Junior', 'Junior'),
+        ('Mid-level', 'Mid-level'),
+        ('Senior', 'Senior'),
     )
 
     job_choices = (
-        ('Part-time','Part-time'),
-        ('Full-time','Full-time'),
-        ('Remote-work','Remote-work'),
-        ('Internship','Internship'),
+        ('Part-time', 'Part-time'),
+        ('Full-time', 'Full-time'),
+        ('Remote-work', 'Remote-work'),
+        ('Internship', 'Internship'),
     )
 
     developer_choices = (
-            ('.NET','.NET'),
-            ('React','React'),
-            ('Nodejs','Nodejs'),
-            ('PHP','PHP'),
-            ('Laravel','Laravel'),
-            ('Angular','Angular'),
-            ('Django','Django'),
-        )
+        ('.NET', '.NET'),
+        ('React', 'React'),
+        ('Nodejs', 'Nodejs'),
+        ('PHP', 'PHP'),
+        ('Laravel', 'Laravel'),
+        ('Angular', 'Angular'),
+        ('Django', 'Django'),
+    )
 
     name = models.CharField(max_length=255)
-    photo = models.ImageField(upload_to = 'media/coders/')
+    photo = models.ImageField(upload_to='media/coders/')
     video_url = models.CharField(max_length=255)
     description = RichTextField()
     city = models.CharField(max_length=255)
     level_type = models.CharField(max_length=255, choices=level_choices)
-    job_type = models.CharField(max_length=255,choices=job_choices)
-    developer_type = models.CharField(max_length=255,choices=developer_choices)
+    job_type = models.CharField(max_length=255, choices=job_choices)
+    developer_type = models.CharField(
+        max_length=255, choices=developer_choices)
     is_featured = models.BooleanField(default=False)
-    created_date = models.DateTimeField(default=datetime.now,blank=True)
+    created_date = models.DateTimeField(default=datetime.now, blank=True)
+    skills = models.ManyToManyField(Skill)
 
     def __str__(self):
         return self.name
