af65af6a1d62100f2f7c3fbd582c2349cc5d4200
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-04-12 00:07:26 +0545

    Fix urls and views for jobs search

--- jobs/urls.py
+++ jobs/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.jobs, name="jobs"),
     path('<int:id>/', views.job_detail, name="job_detail"),
+    path('search/', views.jobs_search, name="jobs_search"),
     # path("job-form/", views.job_form, name="job_form"),
-    # path('search/', views.search, name="search"),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -19,7 +19,6 @@ def jobs(request):
         'developer_type_search': developer_type_search,
         'level_type_search': level_type_search,
         'job_type_search': job_type_search,
-
     }
 
     return render(request, 'jobs/jobs.html', data)
@@ -39,5 +38,42 @@ def job_detail(request, id):
     return render(request, 'jobs/job_detail.html', data)
 
 
-# def search(request):
-#     pass
+def jobs_search(request):
+    jobs = Job.objects.order_by('-created_date')
+    # returns distinct array with field 'developer_type' from model
+    developer_type_search = Job.objects.values_list(
+        'developer_type', flat=True).distinct()
+    level_type_search = Job.objects.values_list(
+        'level_type', flat=True).distinct()
+    job_type_search = Job.objects.values_list(
+        'job_type', flat=True).distinct()
+
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            jobs = jobs.filter(description__icontains=keyword)
+
+    if 'developer_type' in request.GET:
+        developer_type = request.GET['developer_type']
+        if developer_type:
+            jobs = jobs.filter(
+                developer_type__iexact=developer_type)  # exact match
+
+    if 'level_type' in request.GET:
+        level_type = request.GET['level_type']
+        if level_type:
+            jobs = jobs.filter(
+                level_type__iexact=level_type)  # exact match
+
+    if 'job_type' in request.GET:
+        job_type = request.GET['job_type']
+        if job_type:
+            jobs = jobs.filter(job_type__iexact=job_type)  # exact match
+
+    data = {
+        'jobs': jobs,
+        'developer_type_search': developer_type_search,
+        'level_type_search': level_type_search,
+        'job_type_search': job_type_search,
+    }
+    return render(request, 'jobs/jobs_search.html', data)
