74707b88f39c02116aad3dd6687ce5f563988300
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-04-12 17:51:36 +0545

    Add job form url

--- jobs/urls.py
+++ jobs/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", views.jobs, name="jobs"),
     path('<int:id>/', views.job_detail, name="job_detail"),
     path('search/', views.jobs_search, name="jobs_search"),
-    # path("job-form/", views.job_form, name="job_form"),
+    path("job-form/", views.job_form, name="job_form"),
 ]
